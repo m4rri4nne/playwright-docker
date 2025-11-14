798c19dff74f21bc0cbd16091e3dbc40764002e6
Alicia Marianne <aliciadepaulaeng@gmail.com>
2025-11-14 17:28:13 +0000

    fix on logs on setup, and fix on config to run headless

--- global-setup.ts
+++ global-setup.ts
@@ -4,7 +4,7 @@ import path from 'path';
 async function globalSetup() {
     dotenv.config({ path: path.resolve(__dirname, '.env')});
     console.log(process.env.APP_USER_NAME);
-    console.log(process.env.PASSWORD);
+    console.log(process.env.APP_USER_PASSWORD);
 }
 
 export default globalSetup;
